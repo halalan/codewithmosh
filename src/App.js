565bb7da8f01618587582633132208b32214d3b0
halalan <halalanrizkon52@gmail.com>
2019-03-26 12:49:47 +0700

    final arrow function 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 // import  SayHello from "./ Let vs Var vs Const/index";
 // import Object from "./Objects/index";
-import Keyword from "./The this Keyword/index";
+// import Keyword from "./The this Keyword/index";
 // import Binding from "./Binding this/index";
+import Arrow from "./Arrow Functions/index";
 
 
 class App extends Component {
@@ -13,8 +14,9 @@ class App extends Component {
       <div>
    {/* <SayHello /> */}
    {/* <Object /> */}
-   < Keyword/>
+   {/* < Keyword/> */}
    {/* <Binding /> */}
+   <Arrow />
       </div>
     
     );
---
+++ src/Arrow Functions/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+
+class Arrow extends Component {
+  
+  render() {
+
+const square = number => number * number;
+console.log(square(5));
+
+    // const jobs = [
+    //    { id: 1, isActive: true},
+    //    { id: 2, isActive: true},
+    //    { id: 3, isActive: true},
+    // ];
+    
+    // const activeJobs = jobs.filter(function(job) {return job.isActive;});
+    // const activeJobs = jobs.filter(function(job => job.isActive);
+
+    return (
+      
+      <div>
+  
+      </div>
+    
+    );
+  }
+}
+
+
+export default Arrow;
