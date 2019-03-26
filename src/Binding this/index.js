5991a491e091ea646c633778fc5d59ae5732a0ae
halalan <halalanrizkon52@gmail.com>
2019-03-26 11:33:36 +0700

    final Binding in course

--- src/Binding this/index.js
+++ src/Binding this/index.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 
+const person = {
+    name: "Mosh",
+    walk()  {
+        console.log(this)
+    }
+};
+
+person.walk();
+
+const walk = person.walk.bind(person);
+walk();
+
 
 class Binding extends Component {
   
