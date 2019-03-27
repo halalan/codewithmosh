251ac073da518e95a7894e2909426f3f758be019
halalan <halalanrizkon52@gmail.com>
2019-03-27 11:39:10 +0700

    final in Expresions-1

--- src/asset-1/component/Embedding Expressions/counter.js
+++ src/asset-1/component/Embedding Expressions/counter.js
@@ -6,22 +6,17 @@ export default class Counter extends Component {
  count: 0
  };
 
-
- styles = {
-     fontSize:30,
-     fontWeight:"bold"
- }
     render() {
     return (
       <div>
-         <span style={this.styles} className= "badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm" >daftar</button>
+         <span> {this.formatCount()} </span>
+        <button  >daftar</button>
       </div>
     );
   }
 
   formatCount() {
       const {count} = this.state;
-      return count === 0 ? "hero" : count;
+      return count === 0 ? "semangat!!!!" : count;
   }
 }
