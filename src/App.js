5a1c3b8e33aa6eb01a83b24b09d82a5abe8a8b9b
halalan <halalanrizkon52@gmail.com>
2019-03-27 14:57:58 +0700

    final Conditional-Rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import  Cunte from "./asset/component/Updating the State/counter";
-import  Conte from "./asset/component/Binding Event Handlers/counter";
-import  Coun from "./asset/component/Handling Events/counter";
+
+// import  Conte from "./asset/component/Binding Event Handlers/counter";
+// import  Coun from "./asset/component/Handling Events/counter";
 import  Counter from "./asset/component/Conditional Rendering/counter";
-import  Counte from "./asset/component/Passing Event Arguments/counter";
+// import  Counte from "./asset/component/Passing Event Arguments/counter";
 
 class App extends Component {
  
@@ -11,11 +11,12 @@ class App extends Component {
     return (
       
       <div>
-        <Cunte/>
-        <Conte />
+         <Counter/>
+  
+        {/* <Conte />
       <Coun />
-   <Counter/>
-    <Counte />
+  
+    <Counte /> */}
     </div>
 
 
